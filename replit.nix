b96adb382d3f2ec985ff3ecd06b0cba07e39873a
Roman Zhuzha <sanyer@users.noreply.github.com>
2022-04-09 00:15:52 +0000

    add needed ruby packages

--- replit.nix
+++ replit.nix
@@ -1,6 +1,9 @@
 { pkgs }: {
   deps = [
     pkgs.ruby_3_1
+    pkgs.rubyPackages_3_1.nokogiri
+    pkgs.rubyPackages_3_1.racc
+    pkgs.rubyPackages_3_1.jaro_winkler
     pkgs.solargraph
     pkgs.rufo
   ];
