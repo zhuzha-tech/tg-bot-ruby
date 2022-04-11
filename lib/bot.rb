129040ff02c7a729427f57429cb0baa857bde9f7
Roman Zhuzha <sanyer@users.noreply.github.com>
2022-04-11 02:32:43 +0200

    some minor stuff

--- lib/bot.rb
+++ lib/bot.rb
@@ -30,21 +30,21 @@ class MyBot
           when "/start"
             question = "Hi, #{message.from.first_name} \u{1F91D}\u{1F447}!!!"
             buttons = [
-              Telegram::Bot::Types::InlineKeyboardButton.new(text: "МЕДИЦИНА", callback_data: "medicine"),
-              Telegram::Bot::Types::InlineKeyboardButton.new(text: "ВЗАИМООТНОШЕНИЯ", callback_data: "relationship"),
-              Telegram::Bot::Types::InlineKeyboardButton.new(text: "СПОРТ", callback_data: "sport"),
-              Telegram::Bot::Types::InlineKeyboardButton.new(text: "СИЛЬНЫЕ ДУХОМ", callback_data: "willpower"),
-              Telegram::Bot::Types::InlineKeyboardButton.new(text: "ПРАВОСУДИЕ", callback_data: "justice"),
-              Telegram::Bot::Types::InlineKeyboardButton.new(text: "ПСИХОЛОГИЯ,ФИЛОСОФИЯ", callback_data: "psychology"),
-              Telegram::Bot::Types::InlineKeyboardButton.new(text: "ВОЙНА", callback_data: "war"),
-              Telegram::Bot::Types::InlineKeyboardButton.new(text: "НАУКА", callback_data: "science"),
-              Telegram::Bot::Types::InlineKeyboardButton.new(text: "ДЛЯ ДУШИ(не на реальных событиях)", callback_data: "for_the_soul"),
+              Telegram::Bot::Types::InlineKeyboardButton.new(text: "MEDICINE", callback_data: "medicine"),
+              Telegram::Bot::Types::InlineKeyboardButton.new(text: "RELATIONSHIP", callback_data: "relationship"),
+              Telegram::Bot::Types::InlineKeyboardButton.new(text: "SPORT", callback_data: "sport"),
+              Telegram::Bot::Types::InlineKeyboardButton.new(text: "WILLPOWER", callback_data: "willpower"),
+              Telegram::Bot::Types::InlineKeyboardButton.new(text: "JUSTICE", callback_data: "justice"),
+              Telegram::Bot::Types::InlineKeyboardButton.new(text: "PSYCHOLOGY, PHILOSOPHY", callback_data: "psychology"),
+              Telegram::Bot::Types::InlineKeyboardButton.new(text: "WAR", callback_data: "war"),
+              Telegram::Bot::Types::InlineKeyboardButton.new(text: "SCIENCE", callback_data: "science"),
+              Telegram::Bot::Types::InlineKeyboardButton.new(text: "FOR SOUL (not based on real life)", callback_data: "for_the_soul"),
             ]
             keyboard = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: buttons)
             bot.api.send_message(chat_id: message.chat.id, text: question, reply_markup: keyboard)
           when "/stop"
             kb = Telegram::Bot::Types::ReplyKeyboardRemove.new(remove_keyboard: true)
-            bot.api.send_message(chat_id: message.chat.id, text: "Sad, that you leave \u{1F622}. \nI thought we were having fun... \nI hope you'll be back soon \u{1F618} . \nПока \u{1F60A}", reply_markup: kb)
+            bot.api.send_message(chat_id: message.chat.id, text: "Sad, that you leave \u{1F622} \u{1F618} \u{1F60A}", reply_markup: kb)
           when "/status"
             response = ec2_resource.instances
             if response.count.zero?
@@ -52,7 +52,7 @@ class MyBot
             else
               text = ""
               response.each do |instance|
-                text += "#{instance.id} - #{instance.state.name} - #{instance.tags[0].value}"
+                text += "\n#{instance.id} - #{instance.state.name} - #{instance.tags[0].value}"
               end
               bot.api.send_message chat_id: message.chat.id, text: text
             end
